fb990be5fbb0818171483aa95109179bd94acafd
mutiaramarpaung <62175745+mutiaramarpaung@users.noreply.github.com>
2020-03-29 18:01:20 +0700

    Update konversi_menit.js

--- konversi_menit.js
+++ konversi_menit.js
@@ -1,3 +1,5 @@
-function konversiMenit(menit){
-    console.log(menit/60)
-}
+function konversi_menit(num1) {
+    return num1 / 60;
+  }
+var hasil=konversi_menit(65);
+var hasil=console.log(hasil);
