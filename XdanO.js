05d796c47ce8ed9d8731687c8b67527b9271537a
mutiaramarpaung <62175745+mutiaramarpaung@users.noreply.github.com>
2020-03-29 21:26:39 +0700

    Update XdanO.js

--- XdanO.js
+++ XdanO.js
@@ -1,8 +1,11 @@
-function xo(str){
-    if console.log(xo('xoxoxo'))
-    true;
-    else if console.log('oxooxo')
-    false;
-    else if
-    
+function xo(str) {
+    let n = str.length;
+    let count_x = 0;
+    let count_o = 0;
+    for (let i = 0; i < n; i++) {
+        if (str[i] === 'x') count_x++;
+        if (str[i] === 'o') count_o++;
+    }
+
+    return count_x === count_o;
 }
