53768c093032a13670f863dd765897ef646b47e6
mutiaramarpaung <62175745+mutiaramarpaung@users.noreply.github.com>
2020-03-29 12:20:54 +0700

    Update looping_bintang.js

--- looping_bintang.js
+++ looping_bintang.js
@@ -1,4 +1,22 @@
-var counter='*';
-while (counter % 5) {
-    console.log(counter);
+var bintang='*****';
+/** menyusun barisan bintang */
+for (var i=0; i<bintang.length; i++) {
+    var temp=""
+    console.log(bintang[i]);
+}
+
+/** menyusun barisan bintang dengan nested looping */
+for (var i=0; i<bintang.length; i++) {
+    var temp=""
+    console.log(bintang[i] +'' + bintang[i]+'' + bintang[i]+'' + bintang[i]+'' + bintang[i]);
+}
+
+/** menyusun barisan tangga */
+for (var i=4; i<bintang.length; i++) {
+    var temp=""
+    console.log(bintang[i])
+    console.log(bintang[i] +'' + bintang[i])
+    console.log(bintang[i] +'' + bintang[i]+'' + bintang[i])
+    console.log(bintang[i] +'' + bintang[i]+'' + bintang[i]+'' + bintang[i])
+    console.log(bintang[i] +'' + bintang[i]+'' + bintang[i]+'' + bintang[i]+'' + bintang[i]);
 }
