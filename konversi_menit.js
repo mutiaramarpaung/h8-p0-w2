0b4dfe3f4d41639243158411e5f95a1b2bbabfe0
mutiaramarpaung <62175745+mutiaramarpaung@users.noreply.github.com>
2020-03-29 20:07:16 +0700

    Update konversi_menit.js

--- konversi_menit.js
+++ konversi_menit.js
@@ -1,5 +1,12 @@
+function konversi_jam(num1){
+    return (((num1)-(num1-60))/60);
+}
+var hasil_jam1=konversi_jam(65);
+var hasil_jam=console.log(hasil_jam1+' jam');
+
 function konversi_menit(num1) {
-    return num1 / 60;
+    return (num1 - 60);
   }
-var hasil=konversi_menit(65);
-var hasil=console.log(hasil);
+
+var hasil_menit1=konversi_menit(65);
+var hasil_menit=console.log(hasil_menit1 +' menit');
