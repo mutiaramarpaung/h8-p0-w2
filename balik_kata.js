35afd46e9b1cf49d1a3133dcbf7d5822f0296d14
mutiaramarpaung <62175745+mutiaramarpaung@users.noreply.github.com>
2020-03-29 11:42:07 +0700

    Add files via upload

---
+++ balik_kata.js
@@ -0,0 +1,14 @@
+var word2 = 'Hello World and Coders';
+var balik_word2 = word2[21] + word2[20] + word2[19] + word2[18] + word2[17] + word2[16] + word2[15] + word2[14] + word2[13] + word2[12] + word2[11] + word2[10] + word2[9] + word2[8] + word2[7] + word2[6] + word2[5] + word2[4] + word2[3] + word2[2] + word2[1] +word2[0];
+
+var word3 = 'John Die'
+var balik_word3 = word3[7] + word3[6] + word3[5] + word3[4] + word3[3] + word3[2] + word3[1] +word3[0];
+
+var word4 = 'I am a bookworn'
+var balik_word4 = word4[14] + word4[13] + word4[12] + word4[11] + word4[10] + word4[9] + word4[8] + word4[7] + word4[6] + word4[5] + word4[4] + word4[3] + word4[2] + word4[1] +word4[0];
+
+var word5 = 'Coding is my hobby'
+var balik_word5 = word5[17] +word5[16] +word5[15] +word5[14] + word5[13] + word5[12] + word5[11] + word5[10] + word5[9] + word5[8] + word5[7] + word5[6] + word5[5] + word5[4] + word5[3] + word5[2] + word5[1] +word5[0];
+
+var word6 = 'Super'
+var balik_word6 = word5[4] + word5[3] + word5[2] + word5[1] +word5[0];
