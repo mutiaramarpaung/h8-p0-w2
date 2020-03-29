98e749aec6d51d3b20dc245aa2a0c85afcf7188c
mutiaramarpaung <62175745+mutiaramarpaung@users.noreply.github.com>
2020-03-29 10:27:24 +0700

    Add files via upload

---
+++ bandingkan_angka.js
@@ -0,0 +1,19 @@
+
+function bandingkanAngka(angka1, angka2) {
+    if (angka1 < angka2)
+    {
+        console.log(angka1 < angka2)
+    }
+    else if (angka1 > angka2) {
+        console.log(angka1 < angka2)
+    }
+    else if (angka1 = angka2)
+    {
+        console.log('-1');
+    }
+    else {
+    }
+  }
+
+  bandingkanAngka (5, 5);
+  
