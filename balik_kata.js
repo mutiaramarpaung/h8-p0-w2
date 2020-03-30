b4cb782d771e26c743929910b4f88a9e0468685e
mutiaramarpaung <62175745+mutiaramarpaung@users.noreply.github.com>
2020-03-30 09:49:40 +0700

    Update balik_kata.js

--- balik_kata.js
+++ balik_kata.js
@@ -8,8 +8,8 @@ function balikKata(kata) {
 }
 
 /** mencoba /
-console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
-console.log(balikKata('John Doe')); // eoD nhoJ
-console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
-console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
-console.log(balikKata('Super')); // repuS 
+console.log(balikKata('Hello World and Coders'));
+console.log(balikKata('John Doe'));
+console.log(balikKata('I am a bookworm')); 
+console.log(balikKata('Coding is my hobby')); 
+console.log(balikKata('Super'));
